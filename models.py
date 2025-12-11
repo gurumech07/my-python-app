18669bc13c977d85a1cf8137377de7c02bc7cf59
gurumech07 <gurubasav.yellur@gmail.com>
2025-12-11 01:00:14 +0100

    added wanted spaces

--- models.py
+++ models.py
@@ -8,7 +8,10 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        nullable=False)
     completed = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self) -> str:
